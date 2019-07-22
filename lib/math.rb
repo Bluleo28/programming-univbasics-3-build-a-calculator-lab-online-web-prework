19c58ffa0746fae0f7b2582fcf13243c486d80c8
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-22 03:11:04 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
-def addition(num1, num2)
-
+def ;addition (num1, num2)
+expect(addition(5,4).to) eq.(9)
 end
 
 def subtraction(num1, num2)
-
+expect(subtraction(10,5)).to eq(5)
 end
 
 def division(num1, num2)
-
+expect(division(50,2)0.to eq(25))
 end
 
 def multiplication(num1, num2)
-
+expect(multiplication(4,30)).to eq(120)
 end
 
 def modulo(num1, num2)
-
+expect(modulo(34,5)0.to eq(4)
 end
 
 def square_root(num)
-
+expect(square_root(81)).to eq(9)
 end
